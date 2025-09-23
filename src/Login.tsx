c38c601f4b78c870ff4de84698329b46e44f4e14
BadrJaroui <bjaroui@gmail.com>
2025-09-23 15:11:58 +0200

    slight changes to UI, added forgot password and register hyperlinks

--- src/Login.tsx
+++ src/Login.tsx
@@ -12,13 +12,16 @@ export function LoginCredentials () {
     return (
     <div className="login-input-fields">
         <img className="logo" src="./public/download.png" alt="Calendly logo"/>
-        <input className="login-field" type="text" placeholder="username" onChange={(e : any) => setUsername(e.target.value)}/>
-        <input className="login-field" type="password" placeholder="password" onChange={(e : any) => setPassword(e.target.value)}/>
+        <input className="login-field" type="text" placeholder="Email Address" onChange={(e : any) => setUsername(e.target.value)}/>
+        <input className="login-field" type="password" placeholder="Password" onChange={(e : any) => setPassword(e.target.value)}/>
+        <div className="forgot-password">
+            <a href="">Forgot password?</a>
+        </div>
         <button className="login-button" onClick={HandleLogin}>
             Login
         </button>
-        <a className="forgot-password" href="">Forgot password?</a>
         { show && <p className="test">user: {username} pass: {password}</p>}
+        <p className="register-query">Don't have an account? <a href="">Register</a></p>
     </div>
     )
 }
