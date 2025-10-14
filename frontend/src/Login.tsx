8b4859f9f565d1ed9d4a17575973da26215876b9
BadrJaroui <bjaroui@gmail.com>
2025-10-14 15:30:40 +0200

    added thing

---
+++ frontend/src/Login.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export function LoginCredentials () {
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    const [show, setShow] = useState(false);
+
+    function HandleLogin() {
+        setShow(true);
+    }
+
+    return (
+    <div className="login-input-fields">
+        <img className="logo" src="./public/download.png" alt="Calendly logo"/>
+        <input className="login-field" type="text" placeholder="Email Address" onChange={(e : any) => setUsername(e.target.value)}/>
+        <div className="password-and-forgot-password">
+            <input className="login-field" type="password" placeholder="Password" onChange={(e : any) => setPassword(e.target.value)}/>
+            <a href="" className="forgot-password">Forgot password?</a>
+        </div>
+        <button className="login-button" onClick={HandleLogin}>
+            Login
+        </button>
+        { show && <p className="test">user: {username} pass: {password}</p>}
+        <p className="register-query">Don't have an account? <a href="">Register</a></p>
+    </div>
+    )
+}
