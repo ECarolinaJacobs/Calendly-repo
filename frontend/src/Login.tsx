a75130b98700858ac7f3d4ca9f18a8c5b206fcc9
BadrJaroui <bjaroui@gmail.com>
2025-10-15 10:12:52 +0200

    bullshittin

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -11,20 +11,20 @@ export function LoginCredentials () {
     }
 
     return (
-    <div className="login-input-fields">
-        <img className="logo" src="/download.png" alt="Calendly logo"/>
-        <input className="login-field" type="text" placeholder="Email Address" onChange={(e : any) => setUsername(e.target.value)}/>
-        <div className="password-and-forgot-password">
-            <input className="login-field" type="password" placeholder="Password" onChange={(e : any) => setPassword(e.target.value)}/>
-            <a href="" className="forgot-password">Forgot password?</a>
+    <div className="login-page">
+        <div className="login-input-fields">
+            <img className="logo" src="/download.png" alt="Calendly logo"/>
+            <input className="login-field" type="text" placeholder="Email Address" onChange={(e : any) => setUsername(e.target.value)}/>
+            <div className="password-and-forgot-password">
+                <input className="login-field" type="password" placeholder="Password" onChange={(e : any) => setPassword(e.target.value)}/>
+                <a href="" className="forgot-password">Forgot password?</a>
+            </div>
+            <button className="login-button" onClick={HandleLogin}>
+                Login
+            </button>
+            { show && <p className="test">user: {username} pass: {password}</p>}
+            <p className="register-query">Don't have an account? <a href="">Register</a></p>
         </div>
-        <button className="login-button" onClick={HandleLogin}>
-            Login
-        </button>
-        { show && <p className="test">user: {username} pass: {password}</p>}
-        <p className="register-query">Don't have an account? <a href="">Register</a></p>
-
-        <h1>Hello</h1>
     </div>
     )
 }
