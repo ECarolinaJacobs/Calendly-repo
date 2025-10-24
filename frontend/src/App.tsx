f2cfcbe170755b824363813f92fc0a948f0ee86a
Sude <1073976@hr.nl>
2025-10-24 11:58:45 +0200

    Route protection for authentication using session state
    Redirects users that do not have authentication to the login page (which has a placeholder for now, but needs the actual login path later on) see ProtectedPages.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,20 @@
 import { Routes, Route } from "react-router-dom";
 import EventPage from "./events/EventPage";
 import "./App.css"
+import ProtectedRoutes from "./ProtectedFileUtil/ProtectedPages.tsx";
 
 function App() {
   return (
       <Routes>
-        <Route path="/events/:event" element={<EventPage />} />
+        //Routes that do not need authentication can go here for example the login and sign in routes
+
+
+        // Only logged in users can acces the routes below.
+        // Routes that need authentication need to go inside the ProtectedRoutes component
+        <Route element={<ProtectedRoutes/>}>
+          <Route path="/events/:event" element={<EventPage />} />
+          
+        </Route>
       </Routes>
   );
 }
