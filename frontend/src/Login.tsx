bc6fd4ac3517fd1aaff5332ea51391f5348f46a1
BadrJaroui <bjaroui@gmail.com>
2025-10-28 16:04:53 +0100

    Added attempt at API call & CORS policy

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -1,12 +1,31 @@
 import { useState } from "react";
+import verifyData from './api/verify-login.tsx';
 import './App.css'
 
 export function LoginCredentials () {
+    // Maak een User object, die je in de state bewaard, ipv losse strings
+
     const [username, setUsername] = useState();
+    // TODO: Is it a good idea to store username and password in state? Do some researching on this
+    // Password zou denk ik sowieso niet in de state bewaard moeten worden. Username is wel handig denk ik.
     const [password, setPassword] = useState();
     const [show, setShow] = useState(false);
 
+    let authenticated: boolean = false;
+
     function HandleLogin() {
+        if (username !== undefined && password !== undefined) {
+            verifyData(username, password).then((response) => {
+                console.log('test1');
+                if (response !== undefined) {
+                    console.log('test2');
+                    authenticated = true;
+                }
+            })
+        } else {
+            authenticated = false;
+            console.log('test3');
+        }
         setShow(true);
     }
 
@@ -22,7 +41,7 @@ export function LoginCredentials () {
             <button className="login-button" onClick={HandleLogin}>
                 Login
             </button>
-            { show && <p className="test">user: {username} pass: {password}</p>}
+            { show && <p className="test">user: {username} pass: {password} logged in: { authenticated ? "yes" : "no" }</p>}
             <p className="register-query">Don't have an account? <a href="">Register</a></p>
         </div>
     </div>
