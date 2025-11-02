391810cf6325ef9c3341e6265c1de0d6813a01b8
BadrJaroui <bjaroui@gmail.com>
2025-11-02 14:44:18 +0100

    work on booking page

---
+++ frontend/src/pages/BookingPage.tsx
@@ -0,0 +1,49 @@
+import '../BookingPage.css';
+
+export default function BookingLayout() {
+    const rooms = [
+        { name: "Meeting room 1", capacity: 12, availability: "12:00" },
+        { name: "Meeting room 2", capacity: 8, availability: "11:00" }
+    ];
+
+    return (
+        <div className="booking-page">
+            <div className="booking-box">
+                <h1 className="title">Book a meeting room</h1>
+                <div className="filters">
+                    <select className="floor-select" name="floor" id="floor-id">
+                        <option value="">Select floor</option>
+                        <option value="Floor 1">Floor 1</option>
+                        <option value="Floor 2">Floor 2</option>
+                        <option value="Floor 3">Floor 3</option>
+                    </select>
+                    <input type="date"
+                        className="date-select"
+                        name="date-selector"
+                        id="booking-date"
+                        min="2025-01-01"
+                        max="2026-01-01"/>
+                    <button className="filter-button">Filter</button>
+                </div>
+                <div className="available-room-layout">
+                    <table className="booking-table">
+                        <tr>
+                            <th>Rooms</th>
+                            <th>Capacity</th>
+                            <th>Availability</th>
+                        </tr>
+                        <tbody>
+                        {rooms.map(room => (
+                            <tr key={room.name}>
+                            <td>{room.name}</td>
+                            <td>{room.capacity} persons</td>
+                            <td>{room.availability}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
+}
