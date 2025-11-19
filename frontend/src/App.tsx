136ab1526e71402f0b993dce62c9ed432071c649
Elena <1057198@hr.nl>
2025-11-19 01:36:32 +0100

    Dashboard-timetable-addOns

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,18 +10,18 @@ import DashboardPage from "./pages/DashboardPage";
 
 function App() {
   return (
-      <Routes>
+    <Routes>
       {/*Routes that do not need authentication can go here for example the login and sign in routes
         Only logged in users can acces the routes below.
         Routes that need authentication need to go inside the ProtectedRoutes component*/}
-        <Route element={<ProtectedRoutes/>}>
-          <Route path="/events/:event" element={<EventPage />} />
-          <Route path="/dashboard" element={<DashboardPage/>} />
-          <Route path="/booking" element={<BookingPage />} />
-        </Route>
-        <Route path="/login" element={<LoginCredentials />} />
-        <Route path="/register" element={<RegisterPage />}/>
-      </Routes>
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/events/:event" element={<EventPage />} />
+        <Route path="/dashboard" element={<DashboardPage/>} />
+        <Route path="/booking" element={<BookingPage />} />
+      </Route>
+      <Route path="/login" element={<LoginCredentials />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
   );
 }
 
